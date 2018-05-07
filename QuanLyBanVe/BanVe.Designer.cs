e93964804939132776ea5663f29b51758b7b2659
catu29 <38588214+catu29@users.noreply.github.com>
2018-05-08 03:18:45 +0700

    Up Chức năng Bán vé
    Tạm thời đã chạy được. Vẫn chưa hoàn thiện, chưa chạy mượt

--- QuanLyBanVe/BanVe.Designer.cs
+++ QuanLyBanVe/BanVe.Designer.cs
@@ -28,56 +28,182 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel3.SuspendLayout();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.txtCMND = new System.Windows.Forms.TextBox();
+            this.grpBanVe = new System.Windows.Forms.GroupBox();
+            this.dgvVe = new System.Windows.Forms.DataGridView();
+            this.cboHangVe = new System.Windows.Forms.ComboBox();
+            this.btnXacNhan = new System.Windows.Forms.Button();
+            this.btnHuyBo = new System.Windows.Forms.Button();
+            this.lblCMND = new System.Windows.Forms.Label();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.chonMua = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.grpBanVe.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVe)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // txtCMND
             // 
-            this.panel1.Location = new System.Drawing.Point(3, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(291, 456);
-            this.panel1.TabIndex = 0;
+            this.txtCMND.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.txtCMND.Location = new System.Drawing.Point(318, 380);
+            this.txtCMND.MaximumSize = new System.Drawing.Size(1000, 800);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(261, 20);
+            this.txtCMND.TabIndex = 3;
+            this.txtCMND.UseWaitCursor = true;
             // 
-            // panel3
+            // grpBanVe
             // 
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Location = new System.Drawing.Point(300, 4);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(285, 456);
-            this.panel3.TabIndex = 2;
+            this.grpBanVe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpBanVe.Controls.Add(this.dgvVe);
+            this.grpBanVe.Controls.Add(this.cboHangVe);
+            this.grpBanVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpBanVe.Location = new System.Drawing.Point(3, 4);
+            this.grpBanVe.Name = "grpBanVe";
+            this.grpBanVe.Size = new System.Drawing.Size(582, 357);
+            this.grpBanVe.TabIndex = 1;
+            this.grpBanVe.TabStop = false;
+            this.grpBanVe.Text = "Bán vé";
             // 
-            // label1
+            // dgvVe
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(235, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Thông tin khách hàng";
+            this.dgvVe.AllowUserToAddRows = false;
+            this.dgvVe.AllowUserToDeleteRows = false;
+            this.dgvVe.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvVe.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvVe.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvVe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVe.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chonMua});
+            this.dgvVe.GridColor = System.Drawing.SystemColors.Window;
+            this.dgvVe.Location = new System.Drawing.Point(6, 46);
+            this.dgvVe.Name = "dgvVe";
+            this.dgvVe.Size = new System.Drawing.Size(570, 305);
+            this.dgvVe.TabIndex = 1;
+            // 
+            // cboHangVe
+            // 
+            this.cboHangVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboHangVe.FormattingEnabled = true;
+            this.cboHangVe.Items.AddRange(new object[] {
+            "Hạng 1",
+            "Hạng 2"});
+            this.cboHangVe.Location = new System.Drawing.Point(6, 19);
+            this.cboHangVe.Name = "cboHangVe";
+            this.cboHangVe.Size = new System.Drawing.Size(121, 21);
+            this.cboHangVe.TabIndex = 0;
+            this.cboHangVe.Text = "Hạng vé";
+            this.cboHangVe.SelectedIndexChanged += new System.EventHandler(this.cboHangVe_SelectedIndexChanged);
+            // 
+            // btnXacNhan
+            // 
+            this.btnXacNhan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnXacNhan.Location = new System.Drawing.Point(356, 419);
+            this.btnXacNhan.Name = "btnXacNhan";
+            this.btnXacNhan.Size = new System.Drawing.Size(108, 32);
+            this.btnXacNhan.TabIndex = 2;
+            this.btnXacNhan.Text = "Xác nhận đặt mua";
+            this.btnXacNhan.UseVisualStyleBackColor = true;
+            this.btnXacNhan.Click += new System.EventHandler(this.btnXacNhan_Click);
+            // 
+            // btnHuyBo
+            // 
+            this.btnHuyBo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHuyBo.Location = new System.Drawing.Point(471, 419);
+            this.btnHuyBo.Name = "btnHuyBo";
+            this.btnHuyBo.Size = new System.Drawing.Size(108, 32);
+            this.btnHuyBo.TabIndex = 3;
+            this.btnHuyBo.Text = "Hủy bỏ";
+            this.btnHuyBo.UseVisualStyleBackColor = true;
+            this.btnHuyBo.Click += new System.EventHandler(this.btnHuyBo_Click);
+            // 
+            // lblCMND
+            // 
+            this.lblCMND.AutoSize = true;
+            this.lblCMND.Location = new System.Drawing.Point(315, 364);
+            this.lblCMND.Name = "lblCMND";
+            this.lblCMND.Size = new System.Drawing.Size(42, 13);
+            this.lblCMND.TabIndex = 4;
+            this.lblCMND.Text = "CMND:";
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Location = new System.Drawing.Point(242, 419);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(108, 32);
+            this.btnKiemTra.TabIndex = 5;
+            this.btnKiemTra.Text = "Kiểm tra thành viên";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Location = new System.Drawing.Point(6, 364);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(42, 13);
+            this.lblHoTen.TabIndex = 6;
+            this.lblHoTen.Text = "Họ tên:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(9, 380);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(261, 20);
+            this.txtHoTen.TabIndex = 7;
+            // 
+            // chonMua
+            // 
+            this.chonMua.Frozen = true;
+            this.chonMua.HeaderText = "ChonMua";
+            this.chonMua.Name = "chonMua";
+            this.chonMua.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
             // BanVe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(591, 463);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.lblHoTen);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.lblCMND);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.btnHuyBo);
+            this.Controls.Add(this.btnXacNhan);
+            this.Controls.Add(this.grpBanVe);
             this.Name = "BanVe";
             this.Text = "Bán vé";
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
+            this.Load += new System.EventHandler(this.BanVe_Load);
+            this.grpBanVe.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVe)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpBanVe;
+        private System.Windows.Forms.ComboBox cboHangVe;
+        private System.Windows.Forms.DataGridView dgvVe;
+        private System.Windows.Forms.TextBox txtCMND;
+        private System.Windows.Forms.Button btnXacNhan;
+        private System.Windows.Forms.Button btnHuyBo;
+        private System.Windows.Forms.Label lblCMND;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chonMua;
     }
 }
--- QuanLyBanVe/BanVe.cs
+++ QuanLyBanVe/BanVe.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLyBanVe
 {
@@ -15,6 +16,204 @@ namespace QuanLyBanVe
         public BanVe()
         {
             InitializeComponent();
+            
+        }
+
+        private DataTable getVe()
+        {
+            SqlConnection con = new SqlConnection(Properties.Resources.localConnectionString_CamTu);
+            con.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select * From VE", con);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            con.Close();
+
+            return dt;            
+        }
+
+        private DataTable getVe(string MaHV)
+        {
+            SqlConnection con = new SqlConnection(Properties.Resources.localConnectionString_CamTu);
+            con.Open();
+
+            string SQL = string.Format("Select * From VE Where MAHV = '{0}'", MaHV);
+                        
+            SqlDataAdapter da = new SqlDataAdapter(SQL, con);
+
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            con.Close();
+
+            return dt;
+        }
+
+       
+
+        private void BanVe_Load(object sender, EventArgs e)
+        {
+            dgvVe.DataSource = getVe();
+        }
+
+        private void cboHangVe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cboHangVe.SelectedIndex == 0)
+                {
+                    dgvVe.DataSource = getVe("HV001");
+
+                    int demVeDaBan = 0;
+
+                    for (int i = 0; i < dgvVe.Rows.Count; ++i)
+                    {
+                        if (dgvVe["MATT", i].Value.ToString() == "TT000")
+                            break;
+                        else
+                            demVeDaBan++;
+                    }
+
+                    if (demVeDaBan == dgvVe.Rows.Count)
+                    {
+                        MessageBox.Show("Tất cả các vé hạng 1 đã được bán hết.", "Thông báo", MessageBoxButtons.OK);
+                        dgvVe.DataSource = getVe();
+                    }
+                }
+                else if (cboHangVe.SelectedIndex == 1)
+                {
+                    dgvVe.DataSource = getVe("HV002");
+
+                    int demVeDaBan = 0;
+
+                    for (int i = 0; i < dgvVe.Rows.Count; i++)
+                    {
+                        if ("TT001" == dgvVe["MATT", i].Value.ToString())
+                            demVeDaBan++;
+                        else
+                            break;
+                    }
+
+                    if (demVeDaBan == dgvVe.Rows.Count)
+                    {
+                        MessageBox.Show("Tất cả các vé hạng 2 đã được bán hết.", "Thông báo", MessageBoxButtons.OK);
+                        dgvVe.DataSource = getVe();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message.ToString());
+            }
+        }
+        
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnXacNhan_Click(object sender, EventArgs e)
+        {
+            if (txtCMND.Text == "" || txtHoTen.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập thông tin của khách hàng !", "Nhắc nhở", MessageBoxButtons.OK);
+            }            
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Vui lòng kiểm tra thông tin của khách đã đúng hay chưa.", "Nhắc nhở", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)                
+                {
+
+                    using (SqlConnection con = new SqlConnection(Properties.Resources.localConnectionString_CamTu))
+                    { con.Open();
+                        string sql = string.Format("Select * From KHACHHANG Where CMND = '{0}'", txtCMND.Text);
+
+                        SqlDataAdapter da = new SqlDataAdapter(sql, con);
+
+                        DataTable dt = new DataTable();
+
+                        da.Fill(dt);
+
+                        
+                        int demVe = 0;
+
+                        for (int i = 0; i < dgvVe.Rows.Count; ++i)
+                        {
+                            if (dgvVe[0, i].Selected)
+                            {
+                                // Kiểm tra vé đã bán hay chưa
+                                if (dgvVe["MATT", i].Value.ToString() == "TT001")
+                                {
+                                    MessageBox.Show("Vé này đã được bán. Hãy chọn lại một vé khác.", "Thông báo", MessageBoxButtons.OK);
+                                    
+                                }
+                                else
+                                {
+                                    DataRow KH = dt.Rows[dt.Rows.Count - 1];                                    
+
+                                    // Cập nhật vé đã bán
+                                    SqlCommand cmd = new SqlCommand("UpdateTinhTrangVe", con);
+                                    cmd.CommandType = CommandType.StoredProcedure;
+
+                                    cmd.Parameters.AddWithValue("@MaTT", "TT001");
+                                    cmd.Parameters.AddWithValue("@MaVe", dgvVe["MAVE", i].Value.ToString());
+
+                                    cmd.ExecuteNonQuery();
+
+                                    // Cập nhật vé đã bán cho khách hàng nào
+                                    cmd = new SqlCommand("TaoPhieuDatMua", con);
+                                    cmd.CommandType = CommandType.StoredProcedure;
+
+                                    cmd.Parameters.AddWithValue("@MaVe", dgvVe[1, i].Value.ToString());
+                                    cmd.Parameters.AddWithValue("@MaKH", KH["MAKH"].ToString());
+                                    cmd.Parameters.AddWithValue("@ThoiGianDat", DateTime.Now);
+
+                                    cmd.ExecuteNonQuery();
+
+                                    MessageBox.Show("Bán vé thành công !", "Thông báo", MessageBoxButtons.OK);
+                                    dgvVe.DataSource = getVe();
+
+                                    demVe++;
+                                }
+                            }                          
+                        }
+
+                        if (demVe == 0)
+                            MessageBox.Show("Không có vé nào được chọn. Vui lòng chọn 01 vé.", "Cảnh báo", MessageBoxButtons.OK);
+                        
+                        con.Close();
+                    }                    
+                }
+            }
+        }
+
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(Properties.Resources.localConnectionString_CamTu))
+            {
+                con.Open();
+                string sql = string.Format("Select * From KHACHHANG Where CMND = '{0}' and HoTen = N'{1}'", txtCMND.Text, txtHoTen.Text);
+
+                SqlDataAdapter da = new SqlDataAdapter(sql, con);
+
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+              
+                // Kiểm tra khách hàng đã là thành viên hay chưa
+                if (dt.Rows.Count != 0)
+                    MessageBox.Show("Đã là thành viên", "Kết quả kiểm tra", MessageBoxButtons.OK);
+                else
+                {
+                    TaoThanhVien ttv = new TaoThanhVien();
+                    ttv.ShowDialog();
+                }
+
+                con.Close();
+            }
         }
     }
 }
